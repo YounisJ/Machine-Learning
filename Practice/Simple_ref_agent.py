1cade50a304fbcfd1a3f71d87cd4bcb47af17948
YounisJ <muhammadyounis.bscskdkf22@iba-suk.edu.pk>
2025-03-15 13:24:19 +0500

    File Updated

---
+++ Practice/Simple_ref_agent.py
@@ -0,0 +1,29 @@
+class RoomCleaner:
+    def __init__(self):
+        self.location = "Room1"
+        self.environment = {
+            "Room1":"Dirty",
+            "Room2":"Dirty"          
+        }
+    
+    def perceive(self):
+        return self.environment[self.location]
+    
+    def clean(self):
+        if self.perceive() == "Dirty":
+            print(f"Cleaning the {self.location}")
+            self.environment[self.location] = "Clean"
+        else:
+            print(f"{self.location} is cleaned Moving Towards Next Room")
+            if self.location == "Room1":
+                self.location = "Room2"
+            else:
+                self.location = "Room1"
+    
+    def Run_Cleaner(self, steps = 2):
+        for _ in range(steps):
+            self.clean()
+    
+room_cleaner = RoomCleaner()
+
+room_cleaner.Run_Cleaner(4)
